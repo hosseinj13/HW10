02c37ace22ed960d16070bccd7a008ad7b40b658
hossein javadi <hosseinj13@yahoo.com>
2024-02-14 15:47:53 +0330

    create WordPermutations.java

---
+++ src/main/java/one/WordPermutations.java
@@ -0,0 +1,12 @@
+package one;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class WordPermutations {
+    public static void main(String[] args) {
+
+    }
+}
